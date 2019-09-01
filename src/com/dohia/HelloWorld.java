451871a3934352038bf1005a757968b2397e35e2
admin <admin@163.com>
2019-09-01 16:43:30 +0800

    ddd

---
+++ src/com/dohia/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.dohia;
+
+public class HelloWorld {
+
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+	public void name() {
+	System.out.println("世界有你真好");
+	}
+}
